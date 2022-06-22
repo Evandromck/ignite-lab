49e1b359a58288e132a599f76284465b108bd32d
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2022-06-21 22:11:39 -0300

    Deploy

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx' /*todos os arquivos que estiver dentro do src da extenção tsx vai receber o tailwindcss.*/ 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
