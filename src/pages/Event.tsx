6f00b52a21218c8aaf16cb04eb1fa6d9cfb5faac
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2022-07-25 21:00:14 -0300

    Deploy Api Subscriber

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -12,9 +12,8 @@ export function Event(){
          <main className="flex flex-1">
          { slug 
             ? <Video lessonSlug={slug} />
-            : <div className="flex-1 flex justify-center  opacity-25 ">
+            : <div className="flex-1 ">           
             
-            <img src="/src/assets/kastrup.jpg" className="mt-0" alt="" />
             </div>   
            
             }
